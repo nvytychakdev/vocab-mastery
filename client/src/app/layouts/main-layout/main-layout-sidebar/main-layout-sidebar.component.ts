20bed08724a13de948ddc0e487f2cd2f71bc7a18
nvytychakdev <n.vytychak@gmail.com>
2025-05-28 22:37:19 +0300

    feat(buttons): update styles

---
+++ client/src/app/layouts/main-layout/main-layout-sidebar/main-layout-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout-sidebar',
+  imports: [],
+  templateUrl: './main-layout-sidebar.component.html',
+  styleUrl: './main-layout-sidebar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainLayoutSidebarComponent { }
