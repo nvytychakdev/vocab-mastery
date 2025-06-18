12d30b38c54acfd6609e81dc50d566bd238f5c11
nvytychakdev <n.vytychak@gmail.com>
2025-06-18 21:26:56 +0300

    feat(ui): added fancy card

--- client/projects/ui/src/lib/card/card.ts
+++ client/projects/ui/src/lib/card/card.ts
@@ -1,24 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+
+type CardType = 'stat' | 'info';
+
+const CardTypes: Record<CardType, string> = {
+  info: 'vm-card-info',
+  stat: 'vm-card-stat',
+};
 
 @Component({
   selector: 'vm-card',
   imports: [],
   template: `
-    <div class="vm-card-title">
-      <ng-content select="[title]"></ng-content>
+    <div class="vm-card-content">
+      <div class="vm-card-title">
+        <ng-content select="[title]"></ng-content>
+      </div>
+
+      <div class="vm-card-value">
+        <ng-content select="[value]"></ng-content>
+      </div>
     </div>
 
-    <div class="vm-card-value">
-      <ng-content select="[value]"></ng-content>
-    </div>
-
-    <div class="vm-class-action">
+    <div class="vm-card-action">
       <ng-content select="[action]"></ng-content>
     </div>
   `,
-  host: {
-    class: 'vm-card',
-  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Card {}
+export class Card {
+  readonly type = input<CardType>('stat');
+
+  @HostBinding('class')
+  get className() {
+    return `vm-card ${CardTypes[this.type()]}`;
+  }
+}
