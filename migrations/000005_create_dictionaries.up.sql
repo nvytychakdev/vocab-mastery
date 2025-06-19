0a046c09d946220e865ee25038d5c34417820f34
nvytychakdev <n.vytychak@gmail.com>
2025-06-19 21:39:00 +0300

    fix(dictionaries): uuid reference migrations

--- migrations/000005_create_dictionaries.up.sql
+++ migrations/000005_create_dictionaries.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS dictionaries (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-    user_id PREFERENCES users (id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users (id) ON DELETE CASCADE,
     name TEXT NOT NULL,
     description TEXT,
     created_at TIMESTAMPTZ DEFAULT NOW()
--- migrations/000007_create_translations.up.sql
+++ migrations/000007_create_translations.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS translations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
-    word_id PREFERENCES words (id) ON DELETE CASCADE,
+    word_id UUID REFERENCES words (id) ON DELETE CASCADE,
     word TEXT NOT NULL,
     language TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW()
