648e1f06bc49a50bc4aec463a93faaaed59b57e0
nvytychakdev <n.vytychak@gmail.com>
2025-12-23 23:28:26 +0200

    fix(db): word_meanings migration schema

--- migrations/000012_create_word_meanings.up.sql
+++ migrations/000012_create_word_meanings.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS word_meanings (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     word_id UUID REFERENCES words (id) ON DELETE CASCADE,
-    part_of_speech UUID REFERENCES parts_of_speech (id),
+    part_of_speech_id UUID REFERENCES parts_of_speech (id),
     definition TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW()
 )
