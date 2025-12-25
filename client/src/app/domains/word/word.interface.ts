befeb98b15f7a403bb513bb2897578f3f2cfec20
nvytychakdev <n.vytychak@gmail.com>
2025-12-25 14:48:19 +0200

    feat(client): word details typography

--- client/src/app/domains/word/word.interface.ts
+++ client/src/app/domains/word/word.interface.ts
@@ -5,7 +5,7 @@ export type WordMeaningBase = { definition: string; partOfSpeech: string };
 export type WordExampleBase = { text: string };
 
 export type WordExample = Entity<WordExampleBase>;
-export type WordMeaning = Entity<WordMeaningBase & { examples: WordExample[]; synonyms: Entity<WordBase>[] }>;
+export type WordMeaning = Entity<WordMeaningBase & { examples: WordExample[]; synonyms?: Entity<WordBase>[] }>;
 export type Word = Entity<WordBase & { meanings: WordMeaning[] }>;
 
 export type WordListItem = Entity<WordBase>;
--- client/src/app/pages/words/words-details/words-details.ts
+++ client/src/app/pages/words/words-details/words-details.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { WordFacade } from '@domain/word/word.facade';
-import { BadgeComponent } from '@vm/ui';
+import { BadgeComponent, Button } from '@vm/ui';
 
 @Component({
   selector: 'app-words-details',
-  imports: [BadgeComponent, RouterLink],
+  imports: [BadgeComponent, RouterLink, Button],
   host: {
     class: 'flex justify-center',
   },
